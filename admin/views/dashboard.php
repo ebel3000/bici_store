60d897549ed3d8f2e2507acc41e88c80606193b9
ebel3000 <bellomoemi@gmail.com>
2024-12-03 15:14:05 -0300

    added img

--- admin/views/dashboard.php
+++ admin/views/dashboard.php
@@ -1,8 +1,13 @@
-<div class="d-flex justify-content-center p-5">
-    
-<div class= "row">
-<h1 class="text-center mb-5 fw-bold" >PANEL DE CONTROL</h1>
-    <h2 class="text-center">Bienvenido Administrador <?= $userData['username'] ?></h2>
+<div class="d-flex justify-content-center align-items-center vh-80 p-5">
+    <div class="container">
+        <div class="row">
+            <div class="col text-center">
+                <h1 class="display-4 mb-5 fw-bold text-primary">PANEL DE CONTROL</h1>
+                <h2 class="text-secondary">Bienvenido Administrador:</h2> 
+                <h2 class= "fw-bold text-primary"> "<?= $userData['username'] ?>"</h2>
+            </div>
+        </div>
+    </div>
 </div>
-</div>
+
 
