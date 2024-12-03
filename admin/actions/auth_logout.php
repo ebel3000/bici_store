a24b020368b7ca3fa87957250fbbc76f1262e4b3
ebel3000 <bellomoemi@gmail.com>
2024-12-03 16:49:05 -0300

    added alerts 2

--- admin/actions/auth_logout.php
+++ admin/actions/auth_logout.php
@@ -4,6 +4,9 @@ require_once "../../functions/autoload.php";
 
 (new Autenticacion())->log_out();
 
+(new Alerta()) -> add_alerta("secondary", "Se ha deslogueado correctamente");
+
+
 header("Location: ../index.php?sec=login");
 
 ?>
--- admin/views/dashboard.php
+++ admin/views/dashboard.php
@@ -1,8 +1,14 @@
-<div class="d-flex justify-content-center align-items-center vh-80 p-5">
+<div class="row mt-5 justify-content-center">
+    <div class="col col-md-5">
+<?=(new Alerta())->get_alertas()?>
+</div>
+</div>
+
+<div class="d-flex justify-content-center align-items-center  p-5">
     <div class="container">
         <div class="row">
             <div class="col text-center">
-                            <?=(new Alerta())->get_alertas()?>
+                            
 
                 <h1 class="display-4 mb-5 fw-bold text-primary">PANEL DE CONTROL</h1>
                 <h2 class="text-secondary">Bienvenido Administrador:</h2> 
